f2c33c2155a8ad6056fc2446f7051b8c47f52c7b
ljy <117028813@qq.com>
2018-03-26 13:20:01 +0800

    menstrual statistics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,74 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  menstrualList
+  newMenstrual
+  averageCycle
 
+  constructor(
+    public navCtrl: NavController,
+    private dataService: DataProvider
+  ) {}
+
+  ionViewDidLoad() {
+    this.menstrualList = JSON.parse(this.dataService.getMenstrual()) || []
+    this.menstrualList.forEach(val => {
+      val.dateObj = new Date(val.date)
+    })
+    this.sortMenstrualList()
+    this.computeMenstrualCycle()
+    this.computeAverageCycle()
+  }
+
+  addMenstrual() {
+    this.menstrualList.push({
+      date: new Date(this.newMenstrual).getTime(),
+      dateObj: new Date(this.newMenstrual)
+    })
+    this.sortMenstrualList()
+    this.computeMenstrualCycle()
+    this.computeAverageCycle()
+    this.dataService.addMenstrual({
+      date: new Date(this.newMenstrual).getTime()
+    })
+    this.newMenstrual = ''
+  }
+
+  deleteMenstrual(item) {
+    this.menstrualList = this.menstrualList.filter(val => val.date !== item.date)
+    this.computeMenstrualCycle()
+    this.computeAverageCycle()
+    this.dataService.deleteMenstrual(item)
+  }
+
+  computeMenstrualCycle() {
+    this.menstrualList.forEach((val, ind, arr) => {
+      if (ind === 0) {
+        val.cycle = null
+      } else {
+        val.cycle = (val.date - arr[ind - 1].date) / 1000 / 60 / 60 /24
+      }
+    })
+  }
+
+  computeAverageCycle() {
+    let cycleAmount = 0
+    this.menstrualList.forEach((val, ind, arr) => {
+      if (val.cycle) {
+        cycleAmount += val.cycle
+      }
+    })
+    if (this.menstrualList.length > 1) {
+      this.averageCycle = cycleAmount / (this.menstrualList.length - 1)
+    } else {
+      this.averageCycle = 28
+    }
+  }
+
+  sortMenstrualList() {
+    this.menstrualList.sort((a, b) => {
+      return a.date - b.date
+    })
   }
 
 }
---
+++ src/providers/data/data.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the DataProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class DataProvider {
+
+  constructor(
+    public http: HttpClient,
+  ) {
+    console.log('Hello DataProvider Provider');
+  }
+
+  getMenstrual() {
+    return localStorage.getItem('menstrual')
+  }
+
+  addMenstrual(data) {
+    const menstrual = JSON.parse(localStorage.getItem('menstrual')) || []
+    menstrual.push(data)
+    localStorage.setItem('menstrual', JSON.stringify(menstrual))
+  }
+
+  deleteMenstrual(item) {
+    let menstrual = JSON.parse(localStorage.getItem('menstrual'))
+    menstrual = menstrual.filter(val => val.date !== item.date)
+    localStorage.setItem('menstrual', JSON.stringify(menstrual))
+  }
+
+}
